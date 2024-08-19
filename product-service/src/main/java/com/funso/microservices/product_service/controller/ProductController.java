bc26b3af004da6800d6e26b6a26068b5d07b8a2e
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-19 11:35:16 +0100

    Update ProductController class

---
+++ product-service/src/main/java/com/funso/microservices/product_service/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.funso.microservices.product_service.controller;
+
+import com.funso.microservices.product_service.dto.ProductRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public createProduct(@RequestBody ProductRequest productRequest) {
+
+    }
+}
---
+++ product-service/src/main/java/com/funso/microservices/product_service/dto/ProductRequest.java
@@ -0,0 +1,4 @@
+package com.funso.microservices.product_service.dto;
+
+public record ProductRequest() {
+}
