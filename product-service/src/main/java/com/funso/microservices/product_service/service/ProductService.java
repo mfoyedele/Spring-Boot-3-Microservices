26b104d407ec625aed4a24d1d85a0bebfe9fff91
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-19 20:10:01 +0100

    Update ProductService class

--- product-service/src/main/java/com/funso/microservices/product_service/service/ProductService.java
+++ product-service/src/main/java/com/funso/microservices/product_service/service/ProductService.java
@@ -1,5 +1,7 @@
 package com.funso.microservices.product_service.service;
 
+import com.funso.microservices.product_service.dto.ProductRequest;
+import com.funso.microservices.product_service.model.Product;
 import com.funso.microservices.product_service.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +10,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
+
+    public void createProduct(ProductRequest productRequest) {
+      Product product = Product.builder()
+              .name(productRequest.getName())
+              .description(productRequest.getDescription())
+              .price(productRequest.getPrice())
+              .build();
+      productRepository.save(product);
+    }
 }
