bbe466b95c894309a72ef60cf59a82e28c37268f
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-19 11:35:16 +0100

    Added ProductController class

---
+++ product-service/src/main/java/com/funso/microservices/product_service/controller/ProductController.java
@@ -0,0 +1,13 @@
+package com.funso.microservices.product_service.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/product")
+@RequiredArgsConstructor
+public class ProductController {
+}
