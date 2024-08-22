26bb59087e72fb1eaac5d9837cb6e270b8dbaec3
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-22 16:40:01 +0100

    Update ProductServiceApplicationTests

--- product-service/src/test/java/com/funso/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/funso/microservices/product/ProductServiceApplicationTests.java
@@ -3,13 +3,16 @@ package com.funso.microservices.product;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
+import org.testcontainers.containers.MongoDBContainer;
 
 @Import(TestcontainersConfiguration.class)
 @SpringBootTest(WebEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
 	@Test
-	void contextLoads() {
+	void shouldCreateProduct() {
 	}
 
 }
