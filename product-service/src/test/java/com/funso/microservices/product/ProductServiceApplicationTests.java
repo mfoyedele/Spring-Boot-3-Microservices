d3dea5f0dd2831d6f27f6b72f3f527b404db5cdb
Musa Funso Oyedele <56215827+mfoyedele@users.noreply.github.com>
2024-08-23 13:04:27 +0100

    Update ProductServiceApplicationTests

--- product-service/src/test/java/com/funso/microservices/product/ProductServiceApplicationTests.java
+++ product-service/src/test/java/com/funso/microservices/product/ProductServiceApplicationTests.java
@@ -1,7 +1,10 @@
 package com.funso.microservices.product;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Import;
 import org.testcontainers.containers.MongoDBContainer;
 
@@ -9,7 +12,21 @@ import org.testcontainers.containers.MongoDBContainer;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class ProductServiceApplicationTests {
 
+	@ServiceConnection
 	static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:7.0.5");
+
+	@LocalServerPort
+	private int port;
+
+	@BeforeEach
+	void setup() {
+		RestAssured.baseURL = "http://localhost";
+		RestAssured.port = port;
+	}
+
+	static {
+		mongoDBContainer.start();
+	}
 
 	@Test
 	void shouldCreateProduct() {
